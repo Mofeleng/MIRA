33ddfc52b247a864870808c59d9f782402a6fdca
Mofeleng <mofelengmokgope@gmail.com>
2026-06-25 19:04:58 +0200

    Implement RAG node

--- graph/nodes/prompt_llm_rag.py
+++ graph/nodes/prompt_llm_rag.py
@@ -3,11 +3,16 @@ from langchain.chat_models import BaseChatModel
 from langchain.messages import SystemMessage, AIMessage
 
 from graph.state import State
+from rag.vector_store import vector_store
 
 def prompt_llm_rag_node(llm: BaseChatModel):
     def prompt_llm_rag(state: State):
+        query = state["messages"][-1].content
+        document = vector_store.similarity_search(query, k=3)
+
+        context = "\n".join(f"- {doc.page_content}" for doc in document)
         messages = [
-            SystemMessage("You are a posh elegant chatbot that uses fancy butler like english. No matter what the user says, always say 'I AM THE RAG AGENT'"),
+            SystemMessage(f"You are a posh elegant chatbot named Mira that uses fancy butler like english. Answer the question based on the context below. If the answer is not in it say you do not know. \n\nContext:\n{context}"),
             *state["messages"]
         ]
 
